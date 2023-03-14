8abd14e28ea474ee17241f297b6942f984b3f01d
Jiří Pisk <104778666+jiripisk@users.noreply.github.com>
2023-03-14 14:30:12 +0100

    first try GetRating

--- GetRating/__init__.py
+++ GetRating/__init__.py
@@ -1,24 +1,10 @@
 import logging
-
 import azure.functions as func
 
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+def main(req: func.HttpRequest, rating: func.DocumentList) -> str:
+    if not rating:
+        logging.warning("Rating id not found")
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        logging.info("Found rating ID, full rating=%s",
+                     rating[0])
+    return 'OK'
