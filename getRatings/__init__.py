df327b3f37c80f3bbddf3f81c55feda6be34a10f
Zdenek Dlouhy <zdenek.dlouhy@kosik.cz>
2023-03-14 16:33:52 +0100

    return rating list

--- getRatings/__init__.py
+++ getRatings/__init__.py
@@ -13,9 +13,15 @@ import azure.functions as func
 def main(req: func.HttpRequest, ratings: func.DocumentList) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     userId = req.params.get('userid')
+   
+    rating_list = []
+
+    for rating in ratings:
+        raiting_dict = {"id": rating[0]['id'],"userId": rating[0]['userId'],"productId": rating[0]['productId'],"timestamp": rating[0]['timestamp'],"locationName": rating[0]['locationName'],"rating": rating[0]['rating'],"userNotes": rating[0]['userNotes'] }
+        rating_list.append(raiting_dict)
 
     return func.HttpResponse(
-        "OK",
+        rating_list,
         status_code=200
     )
 
