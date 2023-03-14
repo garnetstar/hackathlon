1361133a3870db1df1972892a145cc45edc1ab33
Zdenek Dlouhy <zdenek.dlouhy@kosik.cz>
2023-03-14 14:28:08 +0100

    getRatings

---
+++ getRatings/__init__.py
@@ -0,0 +1,35 @@
+import logging
+
+import azure.functions as func
+
+def get_ratings(req: func.HttpRequest, ratings: func.DocumentList) -> str:
+    if not ratings:
+        logging.warning("Rating items not found")
+    else:
+        logging.info("Found rating items %s", len(ratings))
+    return ratings
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    userId = req.params.get('userId')
+    if not userId:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            userId = req_body.get('name')
+
+    if userId:
+
+       ratings = get_ratings()
+
+       return func.HttpResponse(
+             status_code=200
+        )
+    else:
+        return func.HttpResponse(
+             status_code=404
+        )
