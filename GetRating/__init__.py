402acae411481ef271c9e474da8cbc3032d9f70e
Jiří Pisk <104778666+jiripisk@users.noreply.github.com>
2023-03-15 09:44:56 +0100

    return 404 if not found

--- GetRating/__init__.py
+++ GetRating/__init__.py
@@ -5,9 +5,13 @@ import json
 def main(req: func.HttpRequest, documents: func.DocumentList) -> str:
     if not documents:
         logging.warning("Rating id not found")
+        return func.HttpResponse(
+             "Rating not found",
+             status_code=404
+        )
     else:
         logging.info("Found rating ID, full rating=%s",
                      documents[0])
         raiting_dict = {"id": documents[0]['id'],"userId": documents[0]['userId'],"productId": documents[0]['productId'],"timestamp": documents[0]['timestamp'],"locationName": documents[0]['locationName'],"rating": documents[0]['rating'],"userNotes": documents[0]['userNotes'] }
         raiting_json = json.dumps(raiting_dict)
-    return raiting_json
+    return raiting_json 
